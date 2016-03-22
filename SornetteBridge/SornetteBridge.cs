72add97d4c633809f7a7b99bd1fd2402287481c3
minimalgeek <minimalgeek@gmail.com>
2016-03-22 11:55:47 +0100

    Initial commit

---
+++ SornetteBridge/SornetteBridge.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using AmiBroker;
+using AmiBroker.PlugIn;
+using AmiBroker.Utils;
+using LibSornette;
+using MathWorks.MATLAB.NET.Arrays;
+
+namespace Sornette
+{
+    public class SornetteBridge : IndicatorBase
+    {
+        [ABMethod]
+        public ATArray SorNow(ATArray array, float period)
+        {
+            ATArray result = AFAvg.Ma(array, period);
+            return result;
+        }
+
+        [ABMethod]
+        public ATArray SorRoll(ATArray array, float period)
+        {
+            ATArray result = AFAvg.Ma(array, period);
+            return result;
+        }
+
+        [ABMethod]
+        public ATArray SorFix(ATArray array, float period)
+        {
+            SorFixToMatrix();
+            ATArray result = new ATArray();
+            return result;
+        }
+
+        public double[,] SorFixToMatrix()
+        {
+            try
+            {
+                Sornette sornette = new Sornette();
+                MWArray calculation = sornette.ami_sor_fix();
+                double[,] arr = (double[,])((MWNumericArray)calculation).ToArray(MWArrayComponent.Real);
+                return arr;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return null;
+            }
+        }
+    }
+}
---
+++ SornetteBridgeTest/SornetteBridgeTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Sornette;
+
+namespace SornetteTest
+{
+    [TestClass]
+    public class SornetteBridgeTest
+    {
+        [TestMethod]
+        public void TestSorFixToMatrix()
+        {
+            var bridge = new SornetteBridge();
+            var ret = bridge.SorFixToMatrix();
+
+            Console.WriteLine("Unit ready");
+        }
+    }
+}
