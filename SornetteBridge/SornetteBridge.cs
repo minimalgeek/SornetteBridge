8d462628070d407eab94027404e58db1c67bd3db
minimalgeek <minimalgeek@gmail.com>
2016-03-22 15:27:30 +0100

    Working test

--- SornetteBridge/SornetteBridge.cs
+++ SornetteBridge/SornetteBridge.cs
@@ -7,6 +7,19 @@ using LibSornette;
 
 namespace Bridge
 {
+    public class SornetteBridgeParameterObject
+    {
+        public MWArray array1;
+        public MWArray array2;
+        public MWArray array3;
+        public MWArray array4;
+        public MWArray array5;
+        public MWArray array6;
+        public MWArray array7;
+        public MWArray array8;
+        public MWArray array9;
+    }
+
     public class SornetteBridge : IndicatorBase
     {
         [ABMethod]
@@ -26,25 +39,50 @@ namespace Bridge
         [ABMethod]
         public ATArray SorFix(ATArray array, float period)
         {
-            SorFixToMatrix();
+            //SorFixToMatrix();
             ATArray result = new ATArray();
             return result;
         }
 
-        public double[,] SorFixToMatrix()
+        public double[,] SorFixToMatrix(SornetteBridgeParameterObject parameter)
         {
             try
             {
                 Sornette sornette = new Sornette();
-                MWArray calculation = sornette.ami_sor_fix();
-                double[,] arr = (double[,])((MWNumericArray)calculation).ToArray(MWArrayComponent.Real);
-                return arr;
+                MWArray calculation = sornette.ami_sor_fix(
+                    parameter.array1,
+                    parameter.array2,
+                    parameter.array3,
+                    parameter.array4,
+                    parameter.array5,
+                    parameter.array6,
+                    parameter.array7,
+                    parameter.array8,
+                    parameter.array9);
+
+                double[,] ret = CalculationToDoubleMatrix(calculation);
+                return ret;
             }
             catch (Exception e)
             {
                 Console.WriteLine(e.Message);
                 return null;
             }
+        }
+
+        private static double[,] CalculationToDoubleMatrix(MWArray calculation)
+        {
+            double[,] ret = new double[calculation.Dimensions[0], calculation.Dimensions[1]];
+            for (int i = 0; i < calculation.Dimensions[0]; i++)
+            {
+                for (int j = 0; j < calculation.Dimensions[1]; j++)
+                {
+                    ret[i, j] = (double)calculation.ToArray().GetValue(i, j);
+                }
+                i++;
+            }
+
+            return ret;
         }
     }
 }
--- SornetteBridgeTest/SornetteBridgeTest.cs
+++ SornetteBridgeTest/SornetteBridgeTest.cs
@@ -1,19 +1,32 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Bridge;
+using MathWorks.MATLAB.NET.Arrays;
 
 namespace BridgeTest
 {
     [TestClass]
     public class SornetteBridgeTest
     {
+        private SornetteBridge bridge = new SornetteBridge();
+
         [TestMethod]
         public void TestSorFixToMatrix()
         {
-            var bridge = new SornetteBridge();
-            var ret = bridge.SorFixToMatrix();
+            var param = new SornetteBridgeParameterObject();
 
-            Console.WriteLine("Unit ready");
+            param.array1 = new MWNumericArray(new int[] { 1, 2, 3, 4 });
+            param.array2 = new MWNumericArray(new int[] { 10, 20, 30, 40 });
+            param.array3 = new MWNumericArray(new int[] { 11, 12, 35, 49 });
+            param.array4 = new MWObjectArray(1);
+            param.array5 = new MWObjectArray(1);
+            param.array6 = new MWObjectArray(1);
+            param.array7 = new MWObjectArray(1);
+            param.array8 = new MWObjectArray(1);
+            param.array9 = new MWObjectArray(1);
+
+            var ret = bridge.SorFixToMatrix(param);
+            Assert.IsNotNull(ret);
         }
     }
 }
