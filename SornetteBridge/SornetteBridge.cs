e16803415d1b957364af521f58696128c78fe9a4
minimalgeek <minimalgeek@gmail.com>
2016-03-23 12:22:14 +0100

    Finish working version

--- SornetteBridge/SornetteBridge.cs
+++ SornetteBridge/SornetteBridge.cs
@@ -4,6 +4,7 @@ using AmiBroker.PlugIn;
 using AmiBroker.Utils;
 using MathWorks.MATLAB.NET.Arrays;
 using LibSornette;
+using System.Collections.Generic;
 
 namespace Bridge
 {
@@ -18,30 +19,114 @@ namespace Bridge
         public MWArray array7;
         public MWArray array8;
         public MWArray array9;
+
+        public SornetteBridgeParameterObject() {}
+
+        public SornetteBridgeParameterObject(
+            ATArray atArray1, 
+            ATArray atArray2, 
+            float num1,
+            float num2,
+            float num3,
+            float num4,
+            float num5,
+            float num6)
+        {
+            array1 = ATArrayToMWArray(atArray1);
+            array2 = ATArrayToMWArray(atArray2);
+            array3 = new MWObjectArray((double)num1);
+            array4 = new MWObjectArray((double)num2);
+            array5 = new MWObjectArray((double)num3);
+            array6 = new MWObjectArray((double)num4);
+            array7 = new MWObjectArray((double)num5);
+            array8 = new MWObjectArray((double)num6);
+        }
+
+        public SornetteBridgeParameterObject(
+            ATArray atArray1,
+            ATArray atArray2,
+            ATArray atArray3,
+            float num1,
+            float num2,
+            float num3,
+            float num4,
+            float num5,
+            float num6)
+        {
+            array1 = ATArrayToMWArray(atArray1);
+            array2 = ATArrayToMWArray(atArray2);
+            array3 = ATArrayToMWArray(atArray3);
+            array4 = new MWObjectArray((double)num1);
+            array5 = new MWObjectArray((double)num2);
+            array6 = new MWObjectArray((double)num3);
+            array7 = new MWObjectArray((double)num4);
+            array8 = new MWObjectArray((double)num5);
+            array9 = new MWObjectArray((double)num6);
+        }
+
+        private MWArray ATArrayToMWArray(ATArray atArray)
+        {
+            List<double> container = new List<double>();
+            for (int i = 0; i < atArray.Length; i++)
+            {
+                YTrace.Trace("ATArrayToMWArray: " + atArray[i], YTrace.TraceLevel.Information);
+                container.Add((double)atArray[i]);
+            }
+
+            return new MWNumericArray(1, atArray.Length, container.ToArray());
+        }
     }
 
     public class SornetteBridge : IndicatorBase
     {
         [ABMethod]
-        public ATArray SorNow(ATArray array, float period)
+        public ATArray SorNow(
+            ATArray array1,
+            ATArray array2,
+            float num1,
+            float num2,
+            float num3,
+            float num4,
+            float num5,
+            float num6)
         {
-            ATArray result = AFAvg.Ma(array, period);
-            return result;
+            double[,] matrix = SorNowToMatrix(
+                new SornetteBridgeParameterObject(array1, array2, num1, num2, num3, num4, num5, num6));
+            return DoubleMatrixToATArray(matrix);
         }
 
         [ABMethod]
-        public ATArray SorRoll(ATArray array, float period)
+        public ATArray SorRoll(
+            ATArray array1,
+            ATArray array2,
+            ATArray array3,
+            float num1,
+            float num2,
+            float num3,
+            float num4,
+            float num5,
+            float num6)
         {
-            ATArray result = AFAvg.Ma(array, period);
-            return result;
+            double[,] matrix = SorRollToMatrix(
+                new SornetteBridgeParameterObject(array1, array2, array3, num1, num2, num3, num4, num5, num6));
+            return DoubleMatrixToATArray(matrix);
         }
 
         [ABMethod]
-        public ATArray SorFix(ATArray array, float period)
+        public ATArray SorFix(
+            ATArray array1, 
+            ATArray array2, 
+            ATArray array3, 
+            float num1,
+            float num2,
+            float num3,
+            float num4,
+            float num5,
+            float num6)
         {
-            //SorFixToMatrix();
-            ATArray result = new ATArray();
-            return result;
+            double[,] matrix = SorFixToMatrix(
+                new SornetteBridgeParameterObject(array1, array2, array3, num1, num2, num3, num4, num5, num6));
+            return DoubleMatrixToATArray(matrix);
         }
 
         public double[,] SorFixToMatrix(SornetteBridgeParameterObject parameter)
@@ -65,12 +150,63 @@ namespace Bridge
             }
             catch (Exception e)
             {
-                Console.WriteLine(e.Message);
+                YTrace.Trace("SorFixToMatrix: " + e.Message, YTrace.TraceLevel.Information);
                 return null;
             }
         }
 
-        private static double[,] CalculationToDoubleMatrix(MWArray calculation)
+        public double[,] SorRollToMatrix(SornetteBridgeParameterObject parameter)
+        {
+            try
+            {
+                Sornette sornette = new Sornette();
+                MWArray calculation = sornette.ami_sor_roll(
+                    parameter.array1,
+                    parameter.array2,
+                    parameter.array3,
+                    parameter.array4,
+                    parameter.array5,
+                    parameter.array6,
+                    parameter.array7,
+                    parameter.array8,
+                    parameter.array9);
+
+                double[,] ret = CalculationToDoubleMatrix(calculation);
+                return ret;
+            }
+            catch (Exception e)
+            {
+                YTrace.Trace("SorRollToMatrix: " + e.Message, YTrace.TraceLevel.Information);
+                return null;
+            }
+        }
+
+        public double[,] SorNowToMatrix(SornetteBridgeParameterObject parameter)
+        {
+            try
+            {
+                Sornette sornette = new Sornette();
+                MWArray calculation = sornette.ami_sor_now(
+                    parameter.array1,
+                    parameter.array2,
+                    parameter.array3,
+                    parameter.array4,
+                    parameter.array5,
+                    parameter.array6,
+                    parameter.array7,
+                    parameter.array8);
+
+                double[,] ret = CalculationToDoubleMatrix(calculation);
+                return ret;
+            }
+            catch (Exception e)
+            {
+                YTrace.Trace("SorNowToMatrix: " + e.Message, YTrace.TraceLevel.Information);
+                return null;
+            }
+        }
+
+        private double[,] CalculationToDoubleMatrix(MWArray calculation)
         {
             double[,] ret = new double[calculation.Dimensions[0], calculation.Dimensions[1]];
             for (int i = 0; i < calculation.Dimensions[0]; i++)
@@ -78,8 +214,25 @@ namespace Bridge
                 for (int j = 0; j < calculation.Dimensions[1]; j++)
                 {
                     ret[i, j] = (double)calculation.ToArray().GetValue(i, j);
+                    YTrace.Trace("CalculationToDoubleMatrix: " + ret[i, j], YTrace.TraceLevel.Information);
                 }
                 i++;
+            }
+
+            return ret;
+        }
+
+        private ATArray DoubleMatrixToATArray(double[,] matrix)
+        {
+            ATArray ret = new ATArray();
+
+            if (matrix != null)
+            {
+                for (int i = 0; i < matrix.GetLength(1); i++)
+                {
+                    ret[i] = (float)matrix[0, i];
+                    YTrace.Trace("DoubleMatrixToATArray: " + ret[i], YTrace.TraceLevel.Information);
+                }
             }
 
             return ret;
--- SornetteBridgeTest/SornetteBridgeTest.cs
+++ SornetteBridgeTest/SornetteBridgeTest.cs
@@ -15,15 +15,15 @@ namespace BridgeTest
         {
             var param = new SornetteBridgeParameterObject();
 
-            param.array1 = new MWNumericArray(new int[] { 1, 2, 3, 4 });
-            param.array2 = new MWNumericArray(new int[] { 10, 20, 30, 40 });
-            param.array3 = new MWNumericArray(new int[] { 11, 12, 35, 49 });
+            param.array1 = new MWNumericArray(new int[] { 1, 10 });
+            param.array2 = new MWNumericArray(new int[] { 1, 20 });
+            param.array3 = new MWNumericArray(new int[] { 1, 12 });
             param.array4 = new MWObjectArray(1);
-            param.array5 = new MWObjectArray(1);
-            param.array6 = new MWObjectArray(1);
-            param.array7 = new MWObjectArray(1);
-            param.array8 = new MWObjectArray(1);
-            param.array9 = new MWObjectArray(1);
+            param.array5 = new MWObjectArray(2);
+            param.array6 = new MWObjectArray(3);
+            param.array7 = new MWObjectArray(4);
+            param.array8 = new MWObjectArray(5);
+            param.array9 = new MWObjectArray(6);
 
             var ret = bridge.SorFixToMatrix(param);
             Assert.IsNotNull(ret);
